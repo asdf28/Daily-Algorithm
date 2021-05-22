91ece42f0ffbf3633b70182bffec989dc06c3aaf
asdf28 <qkqhemf28@naver.com>
2021-05-22 23:23:04 +0900

    basics

---
+++ BOJ/BOJ_1929_FindPrimeNumber2.py
@@ -0,0 +1,19 @@
+import sys
+import math
+
+N, M = map(int, sys.stdin.readline().rstrip().split())
+
+li = [True]*(M+1)
+
+li[0] = False
+li[1] = False
+
+for i in range(2,int(math.sqrt(M))+1):
+    j = 2
+    while i*j <= M:
+        li[i*j] = False
+        j += 1
+
+for i in range(N, M+1):
+    if li[i]:
+        print(i)
